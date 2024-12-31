b762cb542d763c4c36da237fa9a6591ec9b4cdad
tsuyocica <76638305+tsuyocica@users.noreply.github.com>
2024-12-31 16:44:54 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,7 +5,18 @@ class RoomsController < ApplicationController
   end
 
   def create
-    binding.pry
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
   end
 
 end
